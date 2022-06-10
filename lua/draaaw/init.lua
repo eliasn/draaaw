494b56a250d334a5afa4afb16309c8c925d289de
Ilya Novozhilov <novozhilov@qst.hk>
2022-06-10 19:47:27 +0300

    fix number of draw function arguments

--- lua/draaaw/init.lua
+++ lua/draaaw/init.lua
@@ -1,19 +1,15 @@
 local M = {}
 
-function M.visual_selection_range()
-    local _, start_row, start_col, _ = unpack(vim.fn.getpos("'<"))
-    local _, end_row, end_col, _ = unpack(vim.fn.getpos("'>"))
-    local coords = string.format('[%d %d] - [%d %d]', start_row, start_col, end_row, end_col)
-    print(coords)
-end
-
 function M.draw_box()
     -- use nvim_buf_get_text(buffer, start_row, start_col, end_row, end_col)
     -- end_row inclusive
     -- end_col exclusive
     -- end_col exclusive
     -- vim.cmd('echo "draw_box called"')
-    M.visual_selection_range()
+    local _, start_row, start_col, _ = unpack(vim.fn.getpos("'<"))
+    local _, end_row, end_col, _ = unpack(vim.fn.getpos("'>"))
+    local text = vim.nvim_buf_get_text(0, start_row, start_col, end_row, end_col)
+    print('and the text is "' .. text .. '"')
 end
 
 function M.draw_arrow()
