6b8068eb9ca743a0bbf54843a2195ec4e20da8fd
Ilya Novozhilov <novozhilov@qst.hk>
2022-06-10 19:29:53 +0300

    move file again

--- lua/draaaw/init.lua
+++ lua/draaaw/init.lua
@@ -1,7 +1,19 @@
 local M = {}
 
+function M.visual_selection_range()
+    local _, start_row, start_col, _ = unpack(vim.fn.getpos("'<"))
+    local _, end_row, end_col, _ = unpack(vim.fn.getpos("'>"))
+    local coords = string.format('[%d %d] - [%d %d]', start_row, start_col, end_row, end_col)
+    vim.cmd('echo ' .. coords)
+end
+
 function M.draw_box()
-    vim.cmd('echo "draw_box called"')
+    -- use nvim_buf_get_text(buffer, start_row, start_col, end_row, end_col)
+    -- end_row inclusive
+    -- end_col exclusive
+    -- end_col exclusive
+    -- vim.cmd('echo "draw_box called"')
+    M.visual_selection_range()
 end
 
 function M.draw_arrow()
